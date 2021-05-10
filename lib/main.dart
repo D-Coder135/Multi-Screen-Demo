e0493676d1cc16e543febf707ba7a525ad669ebf
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-11 00:04:33 +0530

    call the Navigator.pushNamed() function inside the onPressed property and pass it the context field and a named page as a parameter to it inside the screen0.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // home: Screen1(),
-      initialRoute: '/',
+      initialRoute: '/ ',
       routes: {
         '/': (context) => Screen0(),
         '/first': (context) => Screen1(),
